edd179bf713aec7b15e97c103bc2c32c5eadcff2
jrs8205 <jarsi@jarsi.org>
2026-08-03 20:11:08 +0300

    Release 1.24

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     alias(libs.plugins.hilt)
 }
 
-val appVersionName = "1.23"
+val appVersionName = "1.24"
 
 // APKs build as ARK-phone-<version>-<variant>.apk so a downloaded file
 // always tells which version it is.
@@ -23,7 +23,7 @@ android {
         applicationId = "org.jarsi.arkphone"
         minSdk = 26
         targetSdk = 36
-        versionCode = 10
+        versionCode = 11
         versionName = appVersionName
     }
 
